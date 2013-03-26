3f749061820206e9a3e2c56821cf8709add555ab
Lance Burgo <lance.burgo@gmail.com>
2013-03-25 20:22:55 -0400

    Update to JS (simplified) and update to overal style

--- js/jscript.js
+++ js/jscript.js
@@ -1,51 +1,27 @@
 $(document).ready(function() {
 				
-	$('#section2-1').hide();
-	$('#section3-1').hide();
-	$('#section4-1').hide();
-	$('#section5-1').hide();
+	$('.inner_section').hide();
+	$('.section_title').hide();
 
-	$('#section_1').css("z-index", "10");
-	
-	$("#section_1 button").click(function(){
-		$('#section1-1').fadeOut(300, function(){
-			$('#section_1').animate({width:'38px'}, 500, function(){
-				$('#section2-1').fadeIn(300);
-				$('#section_1').css("z-index", "0");
-				$('#section_2').css("z-index", "10");
-			});
+	$('#section_1 .inner_section').show();
+	$('#section_1 .section_title').show();
+
+
+
+	$(".section button.next_step").click(function(){
+		$(this).parent().parent(".section").toggleClass("active", 4000);
+		$(this).parent(".inner_section").siblings(".section_title").fadeOut(300);
+		$(this).parent(".inner_section").fadeOut(300, function(){
+			$(this).siblings(".title_area").animate({width:'39px'}, 500);
+			$(this).parent(".section").animate({width:'39px'}, 500, function(){
+				$(this).css("z-index", "auto");
+				$(this).next().css("z-index", "10");
+				$(this).children(".section_number").css("z-index", "11");
+				$(this).next().toggleClass("active", 4000);
+				$(this).next().children(".inner_section").fadeIn(300);
+				$(this).next().children(".section_title").fadeIn(300);
+			}).css('overflow', 'visible');
 		});
 	});
-
-	$("#section_2 button").click(function(){
-		$('#section2-1').fadeOut(300, function(){
-			$('#section_2').animate({width:'39px'}, 500, function(){
-				$('#section3-1').fadeIn(300);
-				$('#section_2').css("z-index", "0");
-				$('#section_3').css("z-index", "10");
-			});
-		});
-	});
-
-	$("#section_3 button").click(function(){
-		$('#section3-1').fadeOut(300, function(){
-			$('#section_3').animate({width:'39px'}, 500, function(){
-				$('#section4-1').fadeIn(300);
-				$('#section_3').css("z-index", "0");
-				$('#section_4').css("z-index", "10");
-			});
-		});
-	});
-
-	$("#section_4 button").click(function(){
-		$('#section4-1').fadeOut(300, function(){
-			$('#section_4').animate({width:'39px'}, 500, function(){
-				$('#section5-1').fadeIn(300);
-				$('#section_4').css("z-index", "0");
-				$('#section_5').css("z-index", "10");
-			});
-		});
-	});
-
 
 });	
