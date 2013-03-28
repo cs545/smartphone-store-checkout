a2f13006ea284e3e22a7dbce8df50995beb41ab4
jigucci <gu_ji@hotmail.com>
2013-03-27 22:28:06 -0300

    Update Validation.js

--- js/Validation.js
+++ js/Validation.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
  $.validator.addMethod("firstname", function(value, element) 
 { 
-return this.optional(element) || /^[a-z -']+$/i.test(value); 
+return this.optional(element) || /[a-zA-z]+([ '-][a-zA-Z]+)*/i.test(value); 
 }, "Please enter a valid First Name.");
  
 $.validator.addMethod("lastname",function(value,element)
  {
- return this.optional(element) || /^[a-z -']+$/i.test(value); 
+ return this.optional(element) || /[a-zA-z]+([ '-][a-zA-Z]+)*/i.test(value); 
 },"Please enter a valid Last Name.");
- 
-// Validate signup form
+
+$.validator.addMethod("AddressLine1",function(value,element)
+ {
+ return this.optional(element) || /^(?=\s*\S).*$/i.test(value); 
+},"Please enter a valid address.");
+
+// Validate billing details form
  $("#billingdetails").validate({
  rules: {
  firstname: "required firstname",
  lastname: "required lastname",
+ AddressLine1: "required AddressLine1",
  },
  });
  });
