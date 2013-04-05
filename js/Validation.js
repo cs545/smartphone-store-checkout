673328a5d358bdf17804cb9b6976b47dd3d9da10
Ji Gu <gu_ji@hotmail.com>
2013-04-05 01:40:58 -0400

    Update shipping details validation

--- js/Validation.js
+++ js/Validation.js
@@ -1,12 +1,56 @@
 $(document).ready(function() {
- $.validator.addMethod("firstname", function(value, element) 
+//Validating inputs in shipping form section
+ $.validator.addMethod("recv_first_name", function(value, element) 
 { 
-return this.optional(element) || /[a-zA-z]+([ '-][a-zA-Z]+)*/i.test(value); 
+return this.optional(element) || /^[a-zA-Z]+([ '-][a-zA-Z]+)?$/i.test(value); 
 }, "Please enter a valid First Name.");
+
+ $.validator.addMethod("recv_last_name",function(value,element)
+ {
+ return this.optional(element) || /^[a-zA-Z]+([ '-][a-zA-Z]+)?$/i.test(value); 
+},"Please enter a valid Last Name.");
+
+$.validator.addMethod("recv_address_1",function(value,element)
+ {
+ return this.optional(element) || /^(?=\s*\S).*$/i.test(value); 
+},"Please enter a valid address.");
+
+$.validator.addMethod("recv_city",function(value,element)
+ {
+ return this.optional(element) || /([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)/i.test(value); 
+},"Please enter a valid city.");
+
+$.validator.addMethod("recv_zip",function(value,element)
+ {
+ return this.optional(element) || /^\d{5}(-\d{4})?$/i.test(value); 
+},"Please enter a valid zip code.");
+
+$.validator.addMethod("recv_telephone",function(value,element)
+ {
+ return this.optional(element) || /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/i.test(value); 
+},"Please enter a valid phone number.");
+
+//Rules in validating inputs in shipping form section
+ $("#shipping_form").validate({
+ rules: {
+ recv_first_name: "required recv_first_name",
+ recv_last_name: "required recv_last_name",
+ recv_address_1: "required recv_address_1",
+ recv_city: "required recv_city",
+ recv_zip: "required recv_zip",
+ recv_telephone: "required recv_telephone",
+ },
+ });
  
+//Validating inputs in billing details form section
+  $.validator.addMethod("firstname", function(value, element) 
+{ 
+return this.optional(element) || /^[a-zA-Z]+([ '-][a-zA-Z]+)?$/i.test(value); 
+}, "Please enter a valid First Name.");
+
 $.validator.addMethod("lastname",function(value,element)
  {
- return this.optional(element) || /[a-zA-z]+([ '-][a-zA-Z]+)*/i.test(value); 
+ return this.optional(element) || /^[a-zA-Z]+([ '-][a-zA-Z]+)?$/i.test(value); 
 },"Please enter a valid Last Name.");
 
 $.validator.addMethod("AddressLine1",function(value,element)
@@ -14,7 +58,8 @@ $.validator.addMethod("AddressLine1",function(value,element)
  return this.optional(element) || /^(?=\s*\S).*$/i.test(value); 
 },"Please enter a valid address.");
 
-// Validate billing details form
+ 
+//Rules in validating inputs in billing details form section
  $("#billingdetails").validate({
  rules: {
  firstname: "required firstname",
