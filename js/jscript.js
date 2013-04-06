e5324b0aa9a2d64f7daed68e371b6a45390f6919
Lance Burgo <lance.burgo@gmail.com>
2013-04-06 18:25:13 -0400

    Straighten out validation for all fields except CC & DW, modified position of error messages to display next to input label, added validation for drop-down boxes & radio buttons

--- js/jscript.js
+++ js/jscript.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
 	var current_section = 1;
-	console.log(current_section);
-				
+
 	$('.inner_section').hide();
 	$('.section_title').hide();
 	$('.section_number').css({ opacity: 0.5 });
@@ -11,10 +10,49 @@ $(document).ready(function() {
 	$('#section_1 .inner_section').show();
 	$('#section_1 .section_title').show();
 
+	/*checks which payment section should be active in section 3 */
+	function check3payment(){
+		if ( $("#trans_type_cc").is(":checked") ) {
+			$("#sec-3-1-dw_payment_info").fadeOut(300, function() {
+				$("#sec-3-1-cc_payment_info").fadeIn(300);
+			});
+			$("#sec-3-1-cc_payment_info input").attr("required", "required");
+		} else if ( $("#trans_type_dw").is(":checked") ) {
+			$("#sec-3-1-cc_payment_info").fadeOut(300, function() {
+				$("#sec-3-1-dw_payment_info").fadeIn(300);
+			});
+			$("#sec-3-1-dw_payment_info input").attr("required", "required");
+		} else {
+			$("#sec-3-1-cc_payment_info").fadeOut(300);
+			$("#sec-3-1-dw_payment_info").fadeOut(300);
+		}
+	}
+
+	/* checks to see if "use same address" is checked in section 3 */
+	function check3address(){
+		console.log("checking address");
+		if ( $(this).is(":checked") ) {
+			$(".bill_address input").attr("readonly", "readonly").addClass("pre-fill");
+			$(".bill_address select").val( $("#recv_state").val() ).attr("disabled", "disabled");
+			$("#bill_first_name").val( $("#recv_first_name").val() );
+			$("#bill_last_name").val( $("#recv_last_name").val() );
+			$("#bill_address_1").val( $("#recv_address_1").val() );
+			$("#bill_address_2").val( $("#recv_address_2").val() );
+			$("#bill_city").val( $("#recv_city").val() );
+			$("#bill_zip").val( $("#recv_zip").val() );
+			$("#bill_telephone").val( $("#recv_telephone").val() );
+  		} else {
+			$(".bill_address input").attr("readonly", "").removeClass("pre-fill").val("");
+			$(".bill_address select").removeAttr("disabled").val("select")
+  		}
+	}
+
+
+
 	/*this function is for all form submissions*/
-	$('#shipping_form').submit(function(event){
+	$('.form_submit').submit(function(event){
 		event.preventDefault();
-		if ( $(this).validate({rules: {	recv_first_name: "required recv_first_name",recv_last_name: "required recv_last_name",	recv_address_1: "required recv_address_1",	recv_city: "required recv_city",recv_zip: "required recv_zip",	recv_telephone: "required recv_telephone",	},	}).form() ) {
+		if ( $(this).validate().form() ) {
 			current_section++;
 			console.log(current_section);
 			$(this).parent().parent(".section").toggleClass("active", 4000);
@@ -32,37 +70,13 @@ $(document).ready(function() {
 					$(this).children(".section_number").css({ opacity: 1.0 });
 				}).css('overflow', 'visible');
 			});
-			} else {
-				console.log("nogo");
+			if (current_section===3) {
+				check3payment();
+				check3address();
+			}
 		}
-
 	});
 
-	$('#billing_form').submit(function(event){
-		event.preventDefault();
-		if ( $("#billingdetails").validate({rules: {bill_first_name: "required bill_first_name",bill_last_name: "required bill_last_name",bill_address_1: "required bill_address_1",},}).form() ) {
-			current_section++;
-			console.log(current_section);
-			$(this).parent().parent(".section").toggleClass("active", 4000);
-			$(this).parent().parent(".section").prev().children(".section_number").css({ opacity: 0.5 });
-			$(this).parent(".inner_section").siblings(".section_title").fadeOut(300);
-			$(this).parent(".inner_section").fadeOut(300, function(){
-				$(this).siblings(".title_area").animate({width:'39px'}, 500);
-				$(this).parent(".section").animate({width:'39px'}, 500, function(){
-					$(this).css("z-index", "auto");
-					$(this).next().css("z-index", "10");
-					$(this).children(".section_number").css("z-index", "11");
-					$(this).next().toggleClass("active", 4000);
-					$(this).next().children(".inner_section").fadeIn(300);
-					$(this).next().children(".section_title").fadeIn(300);
-					$(this).children(".section_number").css({ opacity: 1.0 });
-				}).css('overflow', 'visible');
-			});
-			} else {
-				console.log("nogo");
-		}
-
-	});
 
 	/* this function is for buttons that continue, but do not involve submitting a form */
 	$("button.next_step").click(function(){
@@ -85,6 +99,7 @@ $(document).ready(function() {
 		});
 	});
 
+	/* this function provides backward movements */
 	$("button.back_step").click(function(){
 		current_section--;
 		console.log(current_section);
@@ -105,42 +120,8 @@ $(document).ready(function() {
 	});
 
 
-	
-	/* This function will copy address fields from the shipping info */
-	$("#sec-3-1-same_address input").click(function() {
-		if ( $(this).is(":checked") ) {
-			$(".bill_address input").attr("readonly", "readonly").addClass("pre-fill");
-			$(".bill_address select").val( $("#recv_state").val() ).attr("disabled", "disabled");
-			$("#bill_first_name").val( $("#recv_first_name").val() );
-			$("#bill_last_name").val( $("#recv_last_name").val() );
-			$("#bill_address_1").val( $("#recv_address_1").val() );
-			$("#bill_address_2").val( $("#recv_address_2").val() );
-			$("#bill_city").val( $("#recv_city").val() );
-			$("#bill_zip").val( $("#recv_zip").val() );
-			$("#bill_telephone").val( $("#recv_telephone").val() );
-  		} else {
-			$(".bill_address input").attr("readonly", "").removeClass("pre-fill").val("");
-			$(".bill_address select").removeAttr("disabled").val("select")
-  		}
-	});
-
-	/* This causes the credit or direct withdraw fields to appear */
-	$("#sec-3-1-payment_method input").click(function() {
-		if ( $("#trans_type_cc").is(":checked") ) {
-			$("#sec-3-1-dw_payment_info").fadeOut(300, function() {
-				$("#sec-3-1-cc_payment_info").fadeIn(300);
-			});
-		} else if ( $("#trans_type_dw").is(":checked") ) {
-			$("#sec-3-1-cc_payment_info").fadeOut(300, function() {
-				$("#sec-3-1-dw_payment_info").fadeIn(300);
-			});
-		} else {
-			$("#sec-3-1-cc_payment_info").fadeOut(300);
-			$("#sec-3-1-dw_payment_info").fadeOut(300);
-		}
-
-	});
-
+	$("#sec-3-1-payment_method input").click( check3payment );
+	$("#sec-3-1-same_address input").click( check3address );
 
 });	
 
--- js/validation.js
+++ js/validation.js
@@ -1,49 +1,46 @@
-/*var validateThisForm = function(thisForm) {
-	$(thisForm).validate({
-		rules: {
-			recv_first_name: "required recv_first_name",
-			recv_last_name: "required recv_last_name",
-			recv_address_1: "required recv_address_1",
-			recv_city: "required recv_city",
-			recv_zip: "required recv_zip",
-			recv_telephone: "required recv_telephone",
-		},
-	});
-
-};*/
-
 $(document).ready(function() {
+
+	//change default required message
+	$.validator.messages.required = "required";
+
+	$.validator.addMethod("valueNotEquals", function(value, element, arg)
+	{
+		return arg != value;
+	}, "Value must not equal arg.");
+
+
+
 
 	//Validating inputs in shipping form section
 	$.validator.addMethod("recv_first_name", function(value, element) 
 	{ 
 		return this.optional(element) || /^[a-zA-Z]+([ '-][a-zA-Z]+)?$/i.test(value); 
-	}, "Please enter a valid First Name.");
+	}, "enter a valid name");
 
 	$.validator.addMethod("recv_last_name",function(value,element)
 	{
 		return this.optional(element) || /^[a-zA-Z]+([ '-][a-zA-Z]+)?$/i.test(value); 
-	},"Please enter a valid Last Name.");
+	},"enter a valid name");
 
 	$.validator.addMethod("recv_address_1",function(value,element)
 	{
 		return this.optional(element) || /^(?=\s*\S).*$/i.test(value); 
-	},"Please enter a valid address.");
+	},"enter a valid address");
 
 	$.validator.addMethod("recv_city",function(value,element)
 	{
 		return this.optional(element) || /([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)/i.test(value); 
-	},"Please enter a valid city.");
+	},"enter a valid city");
 
 	$.validator.addMethod("recv_zip",function(value,element)
 	{
 		return this.optional(element) || /^\d{5}(-\d{4})?$/i.test(value); 
-	},"Please enter a valid zip code.");
+	},"enter a valid zip code");
 
 	$.validator.addMethod("recv_telephone",function(value,element)
 	{
 		return this.optional(element) || /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/i.test(value); 
-	},"Please enter a valid phone number.");
+	},"enter a valid phone number");
 
 	//Rules in validating inputs in shipping form section
 	$("#shipping_form").validate({
@@ -52,36 +49,72 @@ $(document).ready(function() {
 			recv_last_name: "required recv_last_name",
 			recv_address_1: "required recv_address_1",
 			recv_city: "required recv_city",
+			recv_state: { valueNotEquals: "default" },
 			recv_zip: "required recv_zip",
 			recv_telephone: "required recv_telephone",
+			ship_op: { valueNotEquals: "default" },
+		}, messages: {
+			recv_state: { valueNotEquals: "required" },
+			ship_op: { valueNotEquals: "required" },
+		}, errorPlacement: function(error, element) {
+			error.insertAfter(element.prev().children("label"));
 		},
+		debug:true
 	});
+
+
+
 
 	//Validating inputs in billing details form section
 	$.validator.addMethod("bill_first_name", function(value, element) 
 	{
 		return this.optional(element) || /^[a-zA-Z]+([ '-][a-zA-Z]+)?$/i.test(value); 
-	}, "Please enter a valid First Name.");
+	},"enter a valid name");
+
 
 	$.validator.addMethod("bill_last_name",function(value,element)
 	{
 		return this.optional(element) || /^[a-zA-Z]+([ '-][a-zA-Z]+)?$/i.test(value); 
-	},"Please enter a valid Last Name.");
+	},"enter a valid name");
+
 
 	$.validator.addMethod("bill_address_1",function(value,element)
 	{
 		return this.optional(element) || /^(?=\s*\S).*$/i.test(value); 
-	},"Please enter a valid address.");
+	},"enter a valid address");
+
+	$.validator.addMethod("bill_city",function(value,element)
+	{
+		return this.optional(element) || /([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)/i.test(value); 
+	},"enter a valid city");
+
+	$.validator.addMethod("bill_zip",function(value,element)
+	{
+		return this.optional(element) || /^\d{5}(-\d{4})?$/i.test(value); 
+	},"enter a valid zip code");
+
+	$.validator.addMethod("bill_telephone",function(value,element)
+	{
+		return this.optional(element) || /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/i.test(value); 
+	},"enter a valid phone number");
 
 
-	//Rules in validating inputs in billing details form section
-	$("#billingdetails").validate({
+	$("#billing_form").validate({
 		rules: {
 			bill_first_name: "required bill_first_name",
 			bill_last_name: "required bill_last_name",
 			bill_address_1: "required bill_address_1",
+			bill_city: "required bill_city",
+			bill_state: { valueNotEquals: "default" },
+			bill_zip: "required bill_zip",
+			bill_telephone: "required bill_telephone",
+			cc_card_type: { valueNotEquals: "default" },
+		}, messages: {
+			bill_state: { valueNotEquals: "required" },
+			cc_card_type: { valueNotEquals: "required" },
+		}, errorPlacement: function(error, element) {
+			error.insertAfter(element.prev().children("label"));
 		},
+		debug:true
 	});
-
-
 });
